9a180e35fdd0b33b3e1417f225f2297672ab40cb
Joey Hage <joecool520@gmail.com>
2017-10-05 13:34:44 -0500

    Adding comment with reasoning for this file.
    Also explains how to use and when to add to it.

--- src/components/commons/index.js
+++ src/components/commons/index.js
@@ -1,3 +1,18 @@
+/* This file allows all common components listed below to be imported into other files with one line.
+   When creating a new common component, please add it below.
+   
+   Example from EventCheckIn.jsx: 
+      import { MemberInfo, FormGroup, Button } from '../commons';
+          vs.
+      import Button from '../commons/Button';
+      import MemberInfo from '../commons/MemberInfo';
+      import FormGroup from '../commons/FormGroup';
+   
+   Note that we do not need to specify /index.js in the path above. 'index' is a special name that 
+   node and webpack look for when resolving an absolute path that is a directory. See the following post for more 
+   information: https://stackoverflow.com/a/35443250/6732128
+*/
+
 export {Button} from './Button';
 export {MemberInfo} from './MemberInfo';
 export {FormGroup} from './FormGroup';
